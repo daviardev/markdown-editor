58d9984135ffd3053416b78bf451fa6b0156bf68
devsilva <jerson.silva99@hotmail.com>
2023-01-24 19:50:50 -0500

    feat: Use component for the show result

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import MarkdownEditor from 'components/MarkdownEditor'
+
+import './App.css'
+
 const App = () => {
   return <>
-  
+    <div className='editor'>
+      <MarkdownEditor handleEditorValueChange={() => ''} />
+    </div>
   </>
 }
 
