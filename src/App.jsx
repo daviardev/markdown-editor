5420d658d2ebc27236082fb5b515cb07d9c25c65
devsilva <jerson.silva99@hotmail.com>
2023-01-24 20:58:11 -0500

    feat: Use preview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
+
+import Preview from 'components/Preview'
 import MarkdownEditor from 'components/MarkdownEditor'
 
 import './App.css'
 
 const App = () => {
+  const [editorCodeValue, setEditorCodeValue] = useState('')
   return <>
     <div className='editor'>
-      <MarkdownEditor handleEditorValueChange={() => ''} />
+      <MarkdownEditor handleEditorValueChange={setEditorCodeValue} />
+      <Preview code={editorCodeValue} />
     </div>
   </>
 }
